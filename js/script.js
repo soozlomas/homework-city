fa850c195e8aa807c794bd7a9df8b53d5c6dc6d8
soozlomas <sooz@Soozs-MacBook-Pro.local>
2015-09-16 18:16:58 +1000

    corrected calling arrays

--- js/script.js
+++ js/script.js
@@ -1,45 +1,44 @@
 $(document).ready(function(){
 
-  var cityImages = ['images/nyc.jpg','images/sf.jpg','images/la.jpg','images/sydney.jpg','images/austin.jpg','images/citpix_skyline.jpg'];
-console.log('clicked 0');
+  var cityImages = ['images/nyc.jpg','images/sf.jpg','images/la.jpg','images/sydney.jpg','images/austin.jpg'];
+  console.log('clicked 0');
 
-// declare a function to change the image depending on the user's input
+  // declare a function to change the image depending on the user's input
   function changeImage(){
-console.log('clicked 1');
+  console.log('clicked 1');
 
-// collect city name that user inputs and store in city
+    // collect city name that user inputs and store in city
     var city = $('#city-type').val();
-console.log('clicked 2');
+    console.log('clicked 2');
 
-//changes the user's input to lowercase so the case actually entered doesn't matter
+    //changes the user's input to lowercase so the case actually entered doesn't matter
     city = city.toLowerCase();
 
     if(city === 'new york' || city === 'new york city' || city === 'nyc'){
-      $('body').css('background', cityImages[0]);
+      //need the plus signs to concatenate the url, the array index, and the brackets
+      $('body').css('background', 'url(' + cityImages[0] + ')');
     }
     else if (city === 'san francisco' || city === 'sf' || city === 'bay area'){
-      $('body').css('background','url(images/sf.jpg)');
+      $('body').css('background', 'url(' + cityImages[1] + ')');
     }
     else if (city === 'los angeles' || city === 'la' || city === 'lax'){
-      $('body').css('background','url(images/la.jpg)');
+      $('body').css('background', 'url(' + cityImages[2] + ')');
     }
     else if (city === 'austin' || city === 'atx'){
-      $('body').css('background','url(images/austin.jpg)');
+      $('body').css('background', 'url(' + cityImages[3] + ')');
     }
     else if (city === 'sydney' || city === 'syd'){
-      $('body').css('background','url(images/sydney.jpg)');
+      $('body').css('background', 'url(' + cityImages[4] + ')');
     }
-console.log('clicked 3');
+    console.log('clicked 3');
 
-// returns the input field to blank
+    // returns the input field to blank
     $('#city-type').val('');
 
-// stop the browser autmatically refereshing the page
-    return false;
   }
 
-// create click handler for button
-    $('#submit-btn').click(changeImage); //is changeimage here a callback function?
-  console.log('clicked 5');
+  // create click handler for button
+  $('#submit-btn').click(changeImage);
+  console.log('clicked 4');
 
 });
