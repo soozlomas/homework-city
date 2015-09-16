34109da6f52e972ca6fb2d3addbac506f20cd1e3
soozlomas <sooz@Soozs-MacBook-Pro.local>
2015-09-16 19:15:15 +1000

    remove console logs

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
 $(document).ready(function(){
 
   var cityImages = ['images/nyc.jpg','images/sf.jpg','images/la.jpg','images/sydney.jpg','images/austin.jpg'];
-  console.log('clicked 0');
 
   // declare a function to change the image depending on the user's input
   function changeImage(){
-  console.log('clicked 1');
 
     /* collect city name that user inputs and store in city
     changes the user's input to lowercase so the case actually entered doesn't matter
     trims extra spaces that the user inputs*/
     var city = $('#city-type').val().toLowerCase().trim();
-    console.log('clicked 2');
 
     if(city === 'new york' || city === 'new york city' || city === 'nyc'){
       //need the plus signs to concatenate the url, the array index, and the brackets
@@ -29,7 +26,6 @@ $(document).ready(function(){
     else if (city === 'sydney' || city === 'syd'){
       $('body').css('background', 'url(' + cityImages[4] + ')');
     }
-    console.log('clicked 3');
 
     // returns the input field to blank
     $('#city-type').val('');
@@ -41,6 +37,5 @@ $(document).ready(function(){
 
   // create click handler for button
   $('#submit-btn').click(changeImage);
-  console.log('clicked 4');
 
 });
